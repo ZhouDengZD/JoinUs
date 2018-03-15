63de59b697a76740743fbcd59e61acaa693d9bef
ZhouDengZD <1587835128@qq.com>
2018-03-15 17:47:07 +0800

    both get and post succeeded,Ajax.js v1.1

--- src/main/java/com/Controller/GetAllController.java
+++ src/main/java/com/Controller/GetAllController.java
@@ -33,7 +33,7 @@ public class GetAllController {
 
     @RequestMapping("/getAllInfo")
     @ResponseBody
-    //使用Ajax.js版
+    //使用Ajax.js版-Post方法
     public JSONArray getAll(@RequestBody String all, HttpServletRequest request){
         System.out.println(all);
         JSONObject j = JSONObject.fromObject(all);
@@ -55,7 +55,29 @@ public class GetAllController {
         System.out.println(jsonArray.toString());
         return jsonArray;
     }
+    //使用Ajax.js版-get方法
+    //  public JSONArray getAll(@RequestParam(value = "rows") String rows1, @RequestParam(value = "pageNo") String pageNo1){
 
+    //     int rows = Integer.parseInt(rows1);
+    //     int pageNo = Integer.parseInt(pageNo1);
+
+    //     Page<UserInfo> sourceCodes = this.sourceCodeService.getSourceCode(pageNo, rows);
+
+    //     //将每一页的内容转换成Json数组
+    //     JSONArray jsonArray = JSONArray.fromObject(sourceCodes.getContent().toArray());
+
+    //     Map<String, Object> map = new HashMap<>();
+    //     //查询记录的总数以及共多少页
+    //     List<UserInfo> userInfoList = userService.findAllUsers();
+    //     map.put("totalSize",userInfoList.size());
+    //     map.put("totalPage",Math.ceil((double)userInfoList.size()/rows));
+    //     //改动
+    //     jsonArray.add(map);
+
+    //     System.out.println(jsonArray.toString());
+    //     return jsonArray;
+    // }
+    
     //最初版：使用jquery的ajax库版
     // public JSONArray getAll(HttpServletRequest request){
 
